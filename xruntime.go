6a624321cd2a63b0c5b56961f986cec2a87f6d75
itang <live.tang@gmail.com>
2013-12-07 08:30:43 +0800

    update

--- xruntime.go
+++ xruntime.go
@@ -16,6 +16,9 @@ func XRuntime() martini.Handler {
 
 func xRuntimeHandler(w http.ResponseWriter, r *http.Request, c martini.Context) {
 	xw := xRuntimeResponseWriter{time.Now(), martini.NewResponseWriter(w)}
+	xw.Before(func(w martini.ResponseWriter) {
+		xw.tryAddXRuntimeHeader()
+	})
 	c.MapTo(xw, (*http.ResponseWriter)(nil))
 
 	c.Next()
@@ -26,6 +29,8 @@ type xRuntimeResponseWriter struct {
 	martini.ResponseWriter
 }
 
+// use martini.ResponseWriter#Before
+/*
 func (xw xRuntimeResponseWriter) WriteHeader(s int) {
 	xw.tryAddXRuntimeHeader()
 	xw.ResponseWriter.WriteHeader(s)
@@ -35,6 +40,7 @@ func (xw xRuntimeResponseWriter) Write(p []byte) (int, error) {
 	xw.tryAddXRuntimeHeader()
 	return xw.ResponseWriter.Write(p)
 }
+*/
 
 func (xw xRuntimeResponseWriter) tryAddXRuntimeHeader() {
 	if len(xw.Header().Get(HeaderXRuntime)) == 0 {
