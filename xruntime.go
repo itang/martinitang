f6f0f6c7cc1724b8845c1425ab0ea7f8789c2bc2
itang <live.tang@gmail.com>
2013-12-10 21:17:16 +0800

    cleanup

--- xruntime.go
+++ xruntime.go
@@ -14,36 +14,15 @@ func XRuntime() martini.Handler {
 	return xRuntimeHandler
 }
 
-func xRuntimeHandler(w http.ResponseWriter, r *http.Request, c martini.Context) {
-	xw := xRuntimeResponseWriter{time.Now(), martini.NewResponseWriter(w)}
+func xRuntimeHandler(w http.ResponseWriter, c martini.Context) {
+	start := time.Now()
+	xw := martini.NewResponseWriter(w)
 	xw.Before(func(w martini.ResponseWriter) {
-		xw.tryAddXRuntimeHeader()
+		if len(xw.Header().Get(HeaderXRuntime)) == 0 {
+			xw.Header().Add(HeaderXRuntime, fmt.Sprintf("%v", time.Since(start)))
+		}
 	})
 	c.MapTo(xw, (*http.ResponseWriter)(nil))
 
 	c.Next()
-}
-
-type xRuntimeResponseWriter struct {
-	start time.Time
-	martini.ResponseWriter
-}
-
-// use martini.ResponseWriter#Before
-/*
-func (xw xRuntimeResponseWriter) WriteHeader(s int) {
-	xw.tryAddXRuntimeHeader()
-	xw.ResponseWriter.WriteHeader(s)
-}
-
-func (xw xRuntimeResponseWriter) Write(p []byte) (int, error) {
-	xw.tryAddXRuntimeHeader()
-	return xw.ResponseWriter.Write(p)
-}
-*/
-
-func (xw xRuntimeResponseWriter) tryAddXRuntimeHeader() {
-	if len(xw.Header().Get(HeaderXRuntime)) == 0 {
-		xw.Header().Add(HeaderXRuntime, fmt.Sprintf("%v", time.Since(xw.start)))
-	}
 }
